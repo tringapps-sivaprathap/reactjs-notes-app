3f7403c3b9437049c3bd9da372c21a4cf4e4c748
Sivaprathap S <102897756+tringapps-sivaprathap@users.noreply.github.com>
2022-05-13 13:06:49 +0530

    files uploaded

---
+++ src/components/Editor.js
@@ -0,0 +1,32 @@
+import ReactMde from "react-mde"
+import Showdown from "showdown"
+import { useState } from "react"
+
+const Editor = ({ currentNote, updateNote }) => {
+    const [selectedTab, setSelectedTab] = useState("write")
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })  
+
+    return (
+        <section className="pane editor">
+            <ReactMde
+                value={currentNote.body}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+        </section>
+    )
+}
+
+export default Editor
